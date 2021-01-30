e1880ea5e406d0039684425ebbb44aa28f222e27
aviv676 <avivzulva@gmail.com>
2021-01-30 16:43:52 +0700

    restapi

--- index.php
+++ index.php
@@ -1,38 +1,76 @@
 <?php
 
-require_once ("vendor/autoload.php");
+require_once("vendor/autoload.php");
+require_once("Connection.php");
 
 use Slim\App;
 use Slim\Container;
 
-$settings = array(
-	"settings" => array(
-		"displayErrorDetails" => true 
-	)
+$setting = array(
+"setting" => array(
+"displayErrorDetails" => true
+
+   )
+
 );
-$container = new Container($settings);
+
+$container = new Container($setting);
 $app = new App($container);
-			
-$app->get("/pesanan", function($request, $response){
-	$parameter = $request->getQueryParams();
-	$umur = $parameter["AlamatPengiriman"];
-	$result = array(
-		"NamaBarang" => $parameter["NamaBarang"],
-		"Ukuran" => $parameter["Ukuran"],
-		"AlamatPengiriman" => $umur
-	);
-	return $response->withJson($result);
+$db = new Connection();
+
+$app->get("/siswa/all", function($request, $response) {
+	$db = new Connection();
+	$query = "SELECT * FROM siswa";
+	$daftar_siswa = $db->fetchAll($query, []);
+	return $response->withJson($daftar_siswa);
 });
 
-$app->post("/login", function($request, $response){
-	$parameter = $request->getParsedBody();
-	$result = array(
-		"e-mail" => $parameter["e-mail"],
-		"password" => $parameter["password"],
-	);
-	return $response->withJson($result);
+$app->get("/siswa/detail", function($request, $response){
+	$db = new Connection();
+	$param = $request->getQueryParams();
+	$id_siswa = $param['id_siswa'];
+	$args = array(":id" => $id_siswa);
+	$query = "SELECT * FROM siswa WHERE id = :id";
+	$siswa = $db->fetch($query, $args);
+	return $response->withJson($siswa);
 });
 
+$app->post("/siswa/add", function($request, $response){
+	$db = new Connection();
+	$param = $request->getParsedBody();
+	$query = "INSERT INTO siswa( nama, kelas, sekolah) VALUES (:nama, :kelas, :sekolah)";
+	$args = [
+		":nama" => $param['nama'],
+		":kelas" => $param['kelas'],
+		":sekolah" => $param['sekolah']
+	];
+	$db->execute($query, $args);
+	return $response->withJson(["message" => "Success !"]);
+});
 
+$app->post("/siswa/edit", function($request, $response){
+	$db = new Connection();
+	$param = $request->getParsedBody();
+	$query = "UPDATE siswa SET nama = :nama, kelas = :kelas, sekolah = :sekolah WHERE id = :id";
+	$args = [
+		":nama" => $param['nama'],
+		":kelas" => $param['kelas'],
+		":sekolah" => $param['sekolah'],
+		":id" => $param['id']
+	];
+	$db->execute($query, $args);
+	return $response->withJson(["message" => "Success !"]);
+});
+
+$app->post("/siswa/delete", function($request, $response){
+	$db = new Connection();
+	$param = $request->getParsedBody();
+	$query = "DELETE FROM siswa WHERE id = :id";
+	$args = [
+		":id" => $param['id']
+	];
+	$db->execute($query, $args);
+	return $response->withJson(["message" => "Success !"]);
+});
 
 $app->run();
